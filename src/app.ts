a3e2c5dfa53c011d73b445ac332ce215e7a52cf6
Gregory Albouy <gregoryalbouy75@gmail.com>
2023-05-15 17:22:52 +0200

    Encapsulate ports logics

--- src/app.ts
+++ src/app.ts
@@ -2,29 +2,36 @@ import server from "fastify";
 
 import { Elm } from "./elm.js"; // not found? run `npm run build:elm`
 
-const initElm = (
-  environment = "development"
-): ElmApp<{
+type MyElmApp = ElmApp<{
   pingReceiver: PortToElm<null>;
   sendPong: PortFromElm<number>;
-}> => Elm.Main.init({ flags: { environment } });
+}>;
 
-const elmApp = initElm(process.env.NODE_ENV);
-const store = { count: 0 };
+const initElm = (environment = "development") => {
+  const elmApp: MyElmApp = Elm.Main.init({ flags: { environment } });
+  const state = { count: 0 };
+
+  elmApp.ports.sendPong.subscribe((count) => {
+    console.log(`Pong from Elm: ${count}`);
+    state.count = count;
+  });
+
+  return {
+    pingElm: () => {
+      elmApp.ports.pingReceiver.send(null);
+      return state.count;
+    },
+  };
+};
+
+const { pingElm } = initElm(process.env.NODE_ENV);
 
 server()
-  .get("/ping", async () => {
-    elmApp.ports.pingReceiver.send(null);
-    return { pong: store.count };
-  })
+  .get("/ping", async () => ({ pong: pingElm() }))
   .listen({ port: 8080 }, (err, address) => {
     if (err) {
       console.error(err);
       process.exit(1);
     }
-    elmApp.ports.sendPong.subscribe((count) => {
-      console.log(`Pong from Elm: ${count}`);
-      store.count = count;
-    });
     console.log(`Server listening at ${address}`);
   });
